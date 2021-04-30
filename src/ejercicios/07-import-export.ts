56fadd87a774a44d53a5f9047f8895d33208cf22
Edwin Peraza <edwinperaza87@gmail.com>
2021-04-29 22:52:34 -0700

    Corregir path de importacion al mover el archivo

--- src/ejercicios/07-import-export.ts
+++ src/ejercicios/07-import-export.ts
@@ -1,4 +1,4 @@
-import { calcularISV, Producto } from './ejercicios/06-desestructuracion-funcion';
+import { calcularISV, Producto } from './06-desestructuracion-funcion';
 
 
 const carritoDeCompras: Producto[] = [
